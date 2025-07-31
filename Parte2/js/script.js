6fcab403dc9f6487b56ef1b35d963299f5ba6514
SDM30 <simondiaz@javeriana.edu.co>
2025-07-31 08:44:59 -0500

    tareas pendientes agregadas al script.js

---
+++ Parte2/js/script.js
@@ -0,0 +1,19 @@
+// Seleccionar articulo
+
+// TODO #0: Crear carrito de compras (lista vacia de articulos)
+
+// TODO #1: Cuando el usuario haga click sobre el artículo de la tienda se debe agregar al carro de compras
+
+// TODO #2: Al hacer hover sobre el logo del carro se debe mostrar la tabla/lista con los articulos seleccionados
+
+// TODO #2.1: Crear tabla de articulos (imagen, nombre, precio, cantidad, subtotal)
+
+// TODO #2.2: Logica de agregar articulos al carro y actualizar la tablas
+
+// TODO #3: Agregar boton vaciar carro
+
+// Agregar un nuevo articulo a la tienda
+
+// TODO #4: Crear tarjeta del nuevo articulo con los datos suministrados por el usuario
+
+// TODO #4.1: Mostrar alerta si el precio es menor a 1000 y no permitir su creacion
