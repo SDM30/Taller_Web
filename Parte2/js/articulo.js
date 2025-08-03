e390286cbd497d39268bc1519f53fe792fac4aca
SDM30 <simondiaz@javeriana.edu.co>
2025-08-03 11:21:41 -0500

    estilo tienda

--- Parte2/js/articulo.js
+++ Parte2/js/articulo.js
@@ -13,7 +13,10 @@ export class Articulo {
   generarArticulosHTML() {
     let html = `
       <article class="producto">
+      <div class="imagen-contenedor">
         <img src="${this.imagenURL}" class="img-producto" alt="Imagen del producto" />
+      </div>
+      <div class="contenido-producto">
         <h3 class="nombre-producto">${this.nombre}</h3>
         <ul class="atributos-producto">
           <li class="CPU"><strong>CPU:</strong> ${this.CPU}</li>
@@ -24,8 +27,9 @@ export class Articulo {
         </ul>
         <p class="precio-producto">$${this.precio}</p>
         <button class="introducir-carrito">Agregar al carrito</button>
-      </article>
-    `;
+      </div>
+    </article>
+  `;
 
     return html;
   }
@@ -46,53 +50,103 @@ export class Articulo {
 
 export const listaArticulos = [
   new Articulo(
-    "Producto 1",
-    "../images/placeholder.jpg",
-    "Intel i5",
-    "8GB",
-    "256GB",
-    "Intel UHD",
-    "2022",
-    1000
-  ),
-  new Articulo(
-    "Producto 2",
-    "../images/placeholder.jpg",
-    "AMD Ryzen 5",
-    "16GB",
-    "512GB",
-    "AMD Radeon",
-    "2023",
-    2000
-  ),
-  new Articulo(
-    "Producto 3",
-    "../images/placeholder.jpg",
-    "Intel i7",
-    "32GB",
-    "1TB",
-    "NVIDIA GTX 1650",
-    "2021",
-    3000
-  ),
-  new Articulo(
-    "Producto 4",
-    "../images/placeholder.jpg",
-    "AMD Ryzen 7",
-    "16GB",
-    "1TB",
-    "NVIDIA RTX 3060",
-    "2023",
-    4000
-  ),
-  new Articulo(
-    "Producto 5",
-    "../images/placeholder.jpg",
-    "Intel i9",
-    "64GB",
-    "2TB",
-    "NVIDIA RTX 4090",
-    "2024",
+    "Altair 8800",
+    "../images/altair8800.jpg",
+    "Intel 8080 @ 2MHz",
+    "256 bytes",
+    "Disquetes externos",
+    "Luces LED",
+    "1975",
     5000
+  ),
+  new Articulo(
+    "Apple II",
+    "../images/appleii.jpg",
+    "MOS 6502 @ 1MHz",
+    "4KB",
+    'Cassette o disquete 5.25"',
+    "280x192",
+    "1977",
+    2500
+  ),
+  new Articulo(
+    "IBM PC 5150",
+    "../images/ibmpc5150.jpeg",
+    "Intel 8088 @ 4.77MHz",
+    "16KB",
+    "160KB",
+    "CGA",
+    "1981",
+    3500
+  ),
+  new Articulo(
+    "Commodore 64",
+    "../images/c64.webp",
+    "MOS 6510 @ 1MHz",
+    "64KB",
+    'Cassette o disquete 5.25"',
+    "VIC-II",
+    "1982",
+    800
+  ),
+  new Articulo(
+    "Sinclair ZX Spectrum",
+    "../images/zxspectrum.jpg",
+    "Z80 @ 3.5MHz",
+    "16KB/48KB",
+    "Cassette",
+    "ULA",
+    "1982",
+    600
+  ),
+  new Articulo(
+    "BBC Micro",
+    "../images/bbcmicro.png",
+    "MOS 6502 @ 2MHz",
+    "16KB (Model A)",
+    'Cassette o disquete 5.25"',
+    "640x256",
+    "1981",
+    1200
+  ),
+  new Articulo(
+    "NEC PC-8801",
+    "../images/necpc8801.jpg",
+    "μPD780C @ 4MHz",
+    "64KB",
+    " 1MB",
+    "640x400 (8 colores)",
+    "1981",
+    1500
+  ),
+  new Articulo(
+    "Dragon 32",
+    "../images/dragon32.jpg",
+    "Motorola 6809E @ 0.89MHz",
+    "32KB",
+    "Cassette",
+    "MC6847",
+    "1982",
+    700
+  ),
+  new Articulo(
+    "Thomson TO7",
+    "../images/thomsonTO7.jpg",
+    "Motorola 6809 @ 1MHz",
+    "8KB",
+    "Cassette",
+    "320x200",
+    "1982",
+    900
+  ),
+  new Articulo(
+    "Electronika BK-0010",
+    "../images/electronikaBK.jpg",
+    "PDP-11 LSI-11 @ 3MHz",
+    "32KB",
+    'Disquete 5.25"',
+    "512x256 (monocromo)",
+    "1985",
+    2000
   ),
 ];
